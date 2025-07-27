04bdee62e695d239f7751e5b1893c2f48c02b248
yogesan66 <yogesan10@gmail.com>
2025-07-28 02:08:21 +0530

    minor changes #2

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -11,15 +11,21 @@ import { reducer } from './components/store/reducers';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
 
+const devtools = !environment.production
+  ? [
+      provideStoreDevtools({
+        maxAge: 25,
+        logOnly: environment.production,
+      }),
+    ]
+  : [];
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideStore({ multiStepFormData: reducer }),
-    provideStoreDevtools({
-      maxAge: 25,
-      logOnly: !environment.production,
-    }),
+    ...devtools,
   ],
 };
